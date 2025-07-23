c2bda726485d5a2b6f3d7c5abde517ce81ebe798
Isaac Escobar <escobarlazo444@gmail.com>
2025-07-23 10:15:45 -0600

    ejercicio js

---
+++ log.js
@@ -0,0 +1,49 @@
+// let edad = 20;
+// let tieneDui  = true;
+
+// let mensaje = `La persona tiene ${edad} años y su DUI es ${tieneDui ? 'válido' : 'no válido'}.`;
+// console.log(mensaje);
+
+// let edad2 = 25;
+// let TieneD = true;
+
+// let puedeVotar = edad2 >= 18 && TieneD ? 'puede votar' : 'no puede votar';
+// console.log(puedeVotar);
+
+let edad3 = 31;
+
+let membresia = true;
+
+let Descuento = edad3 >= 30 || membresia ? `tiene descuento` : `no tiene descuento`;
+console.log(Descuento);
+
+
+let ege = 15;
+ let altura = 1.70;
+
+ let puedeSubir = ege >= 12 && altura >= 1.60 ? 'puede subir' : 'no puede subir';
+console.log(puedeSubir);
+
+
+///////////////////ejercicio practico
+
+let edad =15;
+let entrada = true;
+
+let mensaje = edad >= 13 && entrada ? 'puede entrar' : 'no puede entrar';
+console.log(mensaje);
+
+//
+let ege2 = 61;
+let discapacidad = true;
+
+let descuento2 = ege2 >= 60 || discapacidad ? 'tiene descuento' : 'no tiene descuento';
+console.log(descuento2);
+
+//
+let nota = 8;
+ let recuperacion = true;
+let resultado = nota >= 7 || recuperacion ? 'aprobado' : 'reprobado';
+console.log(resultado);
+
+
